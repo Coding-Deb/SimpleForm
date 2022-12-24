301d47439e58ce107be0df83dcb03bedbf8073d5
Coding-Deb <debanshubrahma1234@gmail.com>
2022-12-24 12:00:22 +0530

    Done

--- frontend/App.js
+++ frontend/App.js
@@ -9,7 +9,6 @@ export default function App() {
   const [password, setpassword] = useState('')
 
   const handleSubmit = (e) => {
-    e.preventDefault();
 
     const login = {
       name, email, password
